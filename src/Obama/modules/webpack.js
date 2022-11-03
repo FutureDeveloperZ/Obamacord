a9dbc8c3bc67546f7b161531b1b534281b34752d
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2022-11-02 22:45:57 -0400

    Started on webpack

--- src/Obama/modules/webpack.js
+++ src/Obama/modules/webpack.js
@@ -1,6 +1,36 @@
-module.exports = class ObamaWebpacks {
-	getModule(filter) {
-		console.log(filter);
-	}
+/* eslint-disable no-undef */
+// Thank u very much to Replugged
+// https://github.com/replugged-org/replugged/blob/rewrite/src/renderer/modules/webpack.ts
 
+let inst;
+let ready = false;
+let signalReady;
+
+// eslint-disable-next-line no-unused-vars
+const waitFor = new Promise(function(resolve) {
+	return (signalReady = function() {
+		ready = true;
+		resolve();
+	});
+});
+
+function startWebpack(webpackChunk) {
+	if (inst !== undefined) {throw 'Womp Womp Womp';}
+	inst = webpackChunk.push([[ Symbol('Obamacord') ], {}, function(r) { return r; }]);
+	webpackChunk.pop();
+	signalReady();
+}
+
+let webpackChunk;
+Object.defineProperty(window, 'webpackChunkdiscord_app', {
+	get: function() { return webpackChunk; },
+	set: function(v) {
+		if(!ready && v?.push !== Array.prototype.push) { startWebpack(v); }
+		webpackChunk = v;
+	},
+	configurable: true,
+});
+
+module.exports = {
+	webpackChunk,
 };
